604b566f219283ebc7dfbca1e7abe8b05d68619d
Matt Biddulph <mb@hackdiary.com>
2018-01-17 12:41:37 -0800

    Pod name via env var

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 const (
@@ -13,7 +14,7 @@ var calls = 0
 
 func HelloWorld(w http.ResponseWriter, r *http.Request) {
 	calls++
-	fmt.Fprintf(w, "Someone has called me %d times.\n", calls)
+	fmt.Fprintf(w, "Someone has called me in pod %s %d times.\n", os.Getenv("MY_POD_NAME"), calls)
 }
 
 func init() {
